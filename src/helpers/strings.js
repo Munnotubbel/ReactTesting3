1de92a8455b5ba4b0dc0ad0c8691fa5a1a49d2f0
Marcus Weissohn Eede <marcus.weissohn@gmail.com>
2020-04-26 18:45:43 +0200

    Added getStringByLanguage

---
+++ src/helpers/strings.js
@@ -0,0 +1,35 @@
+import { string } from "prop-types"
+
+const languageStrings = {
+    en: {
+     congrats: 'Congratulations! You guessed the word!',
+     submit: 'Submit',
+     guessPrompt: 'Try to guess the secret word!',
+     guessInputPlaceholder: 'enter guess',
+     guessColumnHeader: 'Guessed Words',
+     guessedWords: 'Guesses',
+     matchingLettersColumnHeader: 'Matching Letters',
+    },
+    emoji: {
+     congrats: '🎯🎉',
+     submit: '🚀',
+     guessPrompt: '🤔🤫🔤',
+     guessInputPlaceholder: '⌨️🤔',
+     guessedWords: '🤷‍🔤',
+     guessColumnHeader: '🤷‍',
+     matchingLettersColumnHeader: '✅',
+    }
+  }
+
+  function getStringByLanguage(languageCode, stringKey, strings=languageStrings){
+        if(!strings[languageCode] || !strings[languageCode][stringKey]){
+            // fall bacl to english
+            return strings.en[stringKey];
+        }
+        return strings[languageCode][stringKey]
+  }
+
+  // for future mocking
+  export default {
+      getStringByLanguage,
+  }
